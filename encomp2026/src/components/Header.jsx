99b04dc997b878fd516d8d1f808e8e7dcdf91c24
ViniciusSOAlves <vsoliveiraalves@outlook.com>
2026-03-21 11:33:31 -0300

    Primeiro commit: HEADER

---
+++ encomp2026/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import classes from "./Header.module.css"
+
+
+const Header = () => {
+  return (
+
+    <nav className={classes.navbar}>
+      <div className={classes.containerLogo}>
+        <img className={classes.imagem_logo} src="/Logo.png" alt="Logo"/>
+        <div className={classes.escritoEncomp}>
+          <p>ENCOMP</p>
+        </div>
+       </div>
+      
+        <div className={classes.navButtons}>
+          <button>Componente 1</button>
+          <button>Componente 2</button>
+          <button>Componente 3</button>
+        </div>
+        
+    </nav>
+  );
+}
+
+export default Header
