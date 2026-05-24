3fe4114dcf0fc136719adf02e3bd4d7455ffa1ab
ViniciusSOAlves <vsoliveiraalves@outlook.com>
2026-05-24 10:12:12 -0300

    conteudo site

--- encomp2026/src/App.jsx
+++ encomp2026/src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css'
 import Cursos from './components/Cursos'
 import GerenciaCurso from './components/GerenciaCurso'
-import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <header>
-        <img id='fundo' src='fundo.jpg' alt="Imagem de Fundo" />
+      <header className="main-header">
+        <img id='fundo' src='fundoheader.jpeg' alt="Imagem de Fundo" />
 
         <nav className="navbar">
           <div className="containerLogo">
@@ -25,7 +24,7 @@ function App() {
         </nav>
 
         <div id="intro">
-          <img className="logo-central" src='logoBranca.png'></img>
+          <img className="logo-central" src='logoBranca.png' alt="Logo Central" />
           <h1 id='titulo-intro'><span className='cor'>EN</span>COMP 2026</h1>
           <div className="info-detalhes">
             <div className="item-info">
@@ -40,11 +39,74 @@ function App() {
         </div>
       </header>
 
-      <main>
+      <main className="mb-5">
+        <div className="row bg-black text-white p-5 rounded align-items-center mb-5">
+
+          {/* TÍTULO DA SEÇÃO - Agora com < /> estilo tag de código */}
+          <div className="col-12 text-center mb-5">
+            <h1 className="titulo-secao-tag">
+              <span className="cor">&lt;</span>
+              História do ENCOMP
+              <span className="cor">/&gt;</span>
+            </h1>
+          </div>
+
+          {/* COLUNA DA IMAGEM (ESQUERDA) */}
+          <div className="col-12 col-md-5 text-center mb-4 mb-md-0">
+            <img className="img-fluid p-3" src='logoBranca.png' alt="Logo ENCOMP" style={{ maxHeight: '380px' }} />
+          </div>
+
+          {/* COLUNA DO TEXTO COM ÍCONES (DIREITA) */}
+          <div className="col-12 col-md-7">
+            <div className="bloco-historia">
+
+              {/* Parágrafo 1 */}
+              <div className="item-historia mb-4">
+                <p>
+                  O Encontro de Computação (ENCOMP) surgiu em 2015 como uma iniciativa dos estudantes e professores do curso Bacharelado em Ciência da Computação do IFSULDEMINAS - Campus Passos, com o objetivo de aproximar a comunidade acadêmica do mercado de trabalho e das tendências tecnológicas.
+                </p>
+              </div>
+
+              {/* Parágrafo 2 */}
+              <div className="item-historia mb-4">
+
+                <p>
+                  Ao longo dos anos, o evento cresceu e se consolidou como uma das principais conferências de tecnologia da região, atraindo participantes de diversas instituições e profissionais renomados do setor.
+                </p>
+              </div>
+
+              {/* Parágrafo 3 */}
+              <div className="item-historia">
+
+                <p>
+                  Para 2026, o ENCOMP chega à sua 11ª edição com uma programação especial, trazendo temas inovadores como Inteligência Artificial, Cloud Computing, Edição de Vídeo e muito mais.
+                </p>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
       </main>
-
-      <footer>
-
+      <footer class="row">
+        <div class="col-sm-6 mb-3 mb-sm-0">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
       </footer>
     </>
   )
